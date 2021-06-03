94c54e07cc13db26fb06b47e7d8b98e2b7f9e149
Jonas Kirsch <jonas.kirsch42@googlemail.com>
2021-06-03 20:21:25 +0200

    Workable solution.

--- Client/Model/SignalR/SignalRTransferModel.cs
+++ Client/Model/SignalR/SignalRTransferModel.cs
@@ -1,8 +1,10 @@
 ﻿using Domain.Models;
 using Microsoft.AspNetCore.SignalR.Client;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,19 +15,31 @@ namespace Client.Model.SignalR
         public HubConnection Connection;
         public event Action<ChatMessage> RecievedMessage;
 
+        HttpClient HttpClient;
 
-        public SignalRTransferModel(string url)
+
+        public SignalRTransferModel(string url) : this(url, new HttpClient())
+        {
+        }
+
+        public SignalRTransferModel(string url, HttpClient httpClient)
         {
             Connection = new HubConnectionBuilder().WithUrl(url).WithAutomaticReconnect().Build();
             var v = Connection.StartAsync();
             Console.WriteLine(v.Exception);
 
             Connection.On<ChatMessage>("RecieveChatMessage", (message) => RecievedMessage?.Invoke(message));
+            HttpClient = httpClient;
         }
 
         public async Task<T> LoadData<T>(string url)
         {
-            throw new NotImplementedException();
+            var responseMessage = await HttpClient.GetAsync(url).ConfigureAwait(false);
+            var loadedJSON = await responseMessage.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            T data = JsonConvert.DeserializeObject<T>(loadedJSON);
+
+            return data;
         }
 
 
